82b58aa6a5ab80991509145c25692ba39bfc29d5
Tran Truong <chantruong753@gmail.com>
2024-11-05 19:44:34 +0700

    create file

---
+++ Controllers/ApiControllers/JobController.cs
@@ -0,0 +1,11 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Backend.Controllers.ApiControllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class JobController : ControllerBase
+    {
+    }
+}
